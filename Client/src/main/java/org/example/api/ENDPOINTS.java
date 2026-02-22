214a8f6e9dab2acdcff1dfede84b1b8f102de9ad
Marinela1193 <marpersuar2@alu.edu.gva.es>
2026-02-22 22:20:22 +0100

    creación ENDPOINTS

--- Client/src/main/java/org/example/api/ENDPOINTS.java
+++ Client/src/main/java/org/example/api/ENDPOINTS.java
@@ -4,4 +4,9 @@ public class ENDPOINTS {
     static final String SERVER = "http://localhost:8080/";
     public static final String STUDENTS = "/students/";
     static final String API_PATH = "api";
+
+    public static final String CREATE = "add";
+    public static final String DELETE = "delete";
+    public static final String GET = "get";
+    public static final String UPDATE = "update";
 }
